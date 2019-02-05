1954f472a194ccd079e9beb1b6b468092c44b24b
DamianM <damian.michalski@kramp.com>
2019-02-05 10:54:41 +0100

    Webpack - To Do - plugins

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './TodoList.css';
+
+const TodoList = (props) => {
+  let listElements = props.item.map(function(element) {
+    return (
+        <li onClick={() => {props.remove(element.id)}} key={element.id}>
+          <span className="far fa-clock"></span>
+          {element.text}
+        </li>)
+  })
+  return (
+      <ul className={style.TodoList}>{listElements}</ul>
+  )
+}
+
+export default TodoList;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const plugins = [new HtmlWebpackPlugin({
+  template: 'src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})];
 
 module.exports = (env) => {
+  if (env === 'production') {
+    plugins.push(
+        new OptimizeJsPlugin({
+          sourceMap: false
+        })
+    )
+  };
+
   const environment = env || 'production';
 
   return {
@@ -8,7 +23,7 @@ module.exports = (env) => {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'build'),
-      filename: 'app.' + environment + '.bundle.js'
+      filename: 'app.bundle.js'
     },
     module: {
       rules: [
@@ -16,8 +31,11 @@ module.exports = (env) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
-          }
+            loader: "babel-loader",
+            query: {
+              plugins: ['transform-class-properties']
+            }
+          },
         },
         {
           test: /\.css$/,
@@ -32,6 +50,7 @@ module.exports = (env) => {
           ]
         }
       ]
-    }
+    },
+    plugins: plugins
   }
 };
