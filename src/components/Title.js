4cb99eed35f2e372465684d62be67836a3e0c570
DamianM <damian.michalski@kramp.com>
2019-02-05 09:57:08 +0100

    Webpack - To Do

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    return (
+        <div className="title">
+          <h1>To do title</h1>
+          <h2>Tasks to do: {this.props.item.length}</h2>
+        </div>
+    );
+  }
+}
+
+export default Title;
